f7cbdf74a1652d925b44d6ff21d74d10cef72def
Niharika Goulikar <124892559+Niharika0104@users.noreply.github.com>
2024-07-12 11:27:27 +0000

    fixed article page and also added likes

--- app/home/[postId]/page.tsx
+++ app/home/[postId]/page.tsx
@@ -1,32 +1,13 @@
-"use client"
+'use client'
 import Article from "@/Components/HomePageComponents/Article";
-import axios from "axios";
-// import GetPost from "@/Components/GetParams/GetPostId";
-import { useEffect, useState } from "react";
-import { PostInfo } from "@/app/Types/types";
-import {usePathname} from "next/navigation";
+import { usePathname } from "next/navigation";
+
 export default function ArticlePage() {
-  
-    //const postId=GetPost();
-  
-    const [data,setdata]=useState<PostInfo|undefined>(undefined);
-    const post = usePathname().split('home/')[1];
-   
- 
-  
-    useEffect(()=>{
-        const fetchdata=async ()=>{
-        const res= await axios.post("/api/post/getpost",{
-            postId:post
-        })
-        const data:PostInfo=res.data.data;
-        console.log(data)
-        setdata(data);
-        }
-        fetchdata();
-    },[])
+  const router =usePathname();
+  const postid=router.split('home/')[1];
+    
     return (
-     <div><Article postdata={post}/></div>
+     <div><Article postId={postid}/></div>
     );
   }
   
